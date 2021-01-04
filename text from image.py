0dedd80854de3adcce58a0904e2c99bd9cf64272
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 22:27:24 +0530

    Update text from image.py

--- text from image.py
+++ text from image.py
@@ -1,17 +1,13 @@
 from PIL import Image
 import pytesseract
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'     # mention the correct directory of tesseract file
 def recText(filename):
     text = pytesseract.image_to_string(Image.open(filename))
     return text
 
-info = recText('C:\\Users\\Dell\\Pictures\\Screenshots\\img.png')
+info = recText('C:\\Users\\Dell\\Pictures\\Screenshots\\img.png')   # mention the correct directory of an image
 print(info)
 file = open("result.txt","w")
 file.write(info)
 file.close()
-
-
-
-
